8643e724ee4830f6c0d9c063e07003a2dd935641
rondoval <rondoval@gmail.com>
2022-12-12 16:47:44 +0100

    add color modes

--- custom_components/elkbledom/light.py
+++ custom_components/elkbledom/light.py
@@ -41,9 +41,9 @@ class BLEDOMLight(LightEntity):
     def __init__(self, bledomInstance: BLEDOMInstance, name: str, entry_id: str) -> None:
         self._instance = bledomInstance
         self._entry_id = entry_id
-        self._attr_supported_color_modes = {ColorMode.RGB}
+        self._attr_supported_color_modes = {ColorMode.RGB, ColorMode.COLOR_TEMP, ColorMode.WHITE}
         self._attr_supported_features = LightEntityFeature.EFFECT
-        self._color_mode = ColorMode.RGB
+        self._color_mode = ColorMode.WHITE
         self._attr_name = name
         self._effect = None
         self._attr_unique_id = self._instance.mac
@@ -62,6 +62,18 @@ class BLEDOMLight(LightEntity):
     @property
     def is_on(self) -> Optional[bool]:
         return self._instance.is_on
+
+    @property
+    def max_mireds(self):
+        return 100
+
+    @property
+    def min_mireds(self):
+        return 1
+
+    @property
+    def color_temp(self):
+        return self._instance.color_temp
 
     @property
     def effect_list(self):
@@ -116,9 +128,24 @@ class BLEDOMLight(LightEntity):
             await self._instance.turn_on()
             LOGGER.debug("Change color to white, some error with other infrared control interact")
             await self._instance.set_color((255, 255, 255))
+            await self._instance.set_white(255)
 
         if ATTR_BRIGHTNESS in kwargs and kwargs[ATTR_BRIGHTNESS] != self.brightness and self.rgb_color != None:
             await self._instance.set_white(kwargs[ATTR_BRIGHTNESS])
+
+        if ATTR_COLOR_TEMP in kwargs:
+            self._color_mode = ColorMode.COLOR_TEMP
+            if kwargs[ATTR_COLOR_TEMP] != self.color_temp:
+                self._effect = None
+                await self._instance.set_color_temp(kwargs[ATTR_COLOR_TEMP])
+
+        if ATTR_WHITE in kwargs:
+            self._color_mode = ColorMode.WHITE
+            if self.rgb_color != (255, 255, 255):
+                await self._instance.set_color((255, 255, 255))
+            if kwargs[ATTR_WHITE] != self.brightness:
+                self._effect = None
+                await self._instance.set_white(kwargs[ATTR_WHITE])
 
         if ATTR_RGB_COLOR in kwargs:
             self._color_mode = ColorMode.RGB
